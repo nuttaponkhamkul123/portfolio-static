20ee85fe92ccacd3932f9b431b4ede807c935e8c
nuttaponkhamkul123 <aladen0009@gmail.com>
2021-09-24 17:02:52 +0700

    added highlight of navigation bar

--- js/script.js
+++ js/script.js
@@ -5,12 +5,22 @@ var popcat = document.getElementById("popcat");
 var number = document.getElementById("number");
 var button = document.getElementById("clickme");
 var pop = document.getElementById("pop");
+var btn = document.querySelectorAll("a.nav-item");
 
 // just a random junk number
 console.log(Math.ceil(Math.random()*10));
 
 
-
+console.log(btn[1].innerHTML)
+function nav(number){
+    console.log("nav")
+    for(let i = 0 ; i < 3 ; i++){
+        if(i != number){
+            btn[i].classList.remove("active");
+        }
+    }
+    btn[number].classList.add("active")
+}
 function add(){
     console.log(number.innerHTML)
     let temp = parseInt(number.innerHTML);
